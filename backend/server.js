01cf02c01f0746da2bd7e5e92842f430b96beb1f
tanishkaa <taanishkag078@gmail.com>
2022-10-31 01:32:44 +0530

    commit repo

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv")
+
+const app = express();
+dotenv.config();
+
+app.get('/',(req, res) => {
+    res.send("API is running");
+});
+
+app.get('/api/notes', (req,res) => {
+    res.json(notes)
+});
+
+app.get('/api/notes/:id',(req, res) => {
+    const note = notes.find((n) => n._id === req.params.id);
+    res.send(note);
+});
+
+const PORT =process.env.PORT || 4400;
+
+app.listen(PORT, console.log(`server has started on PORT ${PORT}`));
+
